0baf20c636d130e32c470aa03f5f5fe16e425a91
Caner Türe <cnrture@gmail.com>
2021-07-08 17:49:52 +0300

    Interface Links Changes

--- app/src/main/java/com/canerture/booksapp/retrofit/BooksDAOInterface.kt
+++ app/src/main/java/com/canerture/booksapp/retrofit/BooksDAOInterface.kt
@@ -10,10 +10,10 @@ import retrofit2.http.POST
 
 interface BooksDAOInterface {
 
-    @GET("/all_books.php")
+    @GET("default/all_books.php")
     fun allBooks(): Call<BooksResponse>
 
-    @POST("/add_book.php")
+    @POST("default/add_book.php")
     @FormUrlEncoded
     fun addBook(@Field("book_name") book_name: String,
                 @Field("book_author") book_author: String,
@@ -21,11 +21,11 @@ interface BooksDAOInterface {
                 @Field("book_price") book_price: String,
                 @Field("book_image_url") book_image_url: String): Call<CRUDResponse>
 
-    @POST("/search_book.php")
+    @POST("default/search_book.php")
     @FormUrlEncoded
     fun searchBook(@Field("book_name") book_name: String): Call<BooksResponse>
 
-    @POST("/cart_status_change.php")
+    @POST("default/cart_status_change.php")
     @FormUrlEncoded
     fun cartStatusChange(@Field("book_id") book_id: Int,
                          @Field("cart_status") cart_status: Int): Call<CRUDResponse>
