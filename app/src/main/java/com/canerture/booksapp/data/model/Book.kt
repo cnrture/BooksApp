234a23c38402ca71107d428a25a5dec34f02ee29
Caner Türe <cnrture@gmail.com>
2023-08-13 02:02:07 +0300

    update model and refactor design

--- app/src/main/java/com/canerture/booksapp/data/model/Book.kt
+++ app/src/main/java/com/canerture/booksapp/data/model/Book.kt
@@ -12,4 +12,5 @@ data class Book(
     val publisher: String?,
     val price: String?,
     @SerializedName("image_url") val imageUrl: String?,
+    @SerializedName("is_best_seller") val isBestSeller: Boolean?
 ) : Parcelable
